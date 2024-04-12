355b0c33775d5a9d070f69a200417f16a41bf984
robelchane <robel.chane@edu.sait.ca>
2024-04-12 02:28:02 -0600

    Lab 6 Done
    I just connect with the database file which is tasks.json

--- components/ToDoForm.jsx
+++ components/ToDoForm.jsx
@@ -1,54 +1,61 @@
-import React from "react";
-import {
-  TextInput,
-  StyleSheet,
-  Button,
+"use client";
+
+import { useState, useEffect } from "react";
+import tasksData from "./data/tasks.json";
+import { 
+  TextInput, 
+  StyleSheet, 
+  Button, 
   View,
-  Text,
-} from 'react-native';
+  Text } from "react-native";
 
-function ToDoForm({addTask}){
-  const [input,setInput] = React.useState('');
-  const handleChangeText = (text) => {
-    setInput(text);
-  }
-  const handlePress = () => {
-    addTask(input);
-  }
+function ToDoForm({ addTask }) {
+  const [input, setInput] = useState("");
+  const [tasks, setTasks] = useState([]);
 
-    return(
-        <>
-        <View style={styles.form}>
-        <TextInput
-            style={styles.input}
-            placeholder="Add a new task..."
-            onChangeText={handleChangeText}
-            value="input"
-        />
-        <Button title="Add" onPress={handlePress} />
-        </View>
-        </>
-    );
+  useEffect(() => {
+    setTasks(tasksData.tasks);
+  }, []);
+
+  const handleAddTask = () => {
+    const randomIndex = Math.floor(Math.random() * tasks.length);
+    const randomTask = tasks[randomIndex];
+    addTask(randomTask);
+    setInput(randomTask);
+  };
+
+  return (
+    <View style={styles.form}>
+      <TextInput
+        style={styles.input}
+        placeholder="Add a new task..."
+        onChangeText={setInput}
+        value={input}
+      />
+      <Button title="Generate Random Task" onPress={handleAddTask} />
+    </View>
+
+  );
 
 }
 
 const styles = StyleSheet.create({
 
-    form: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      marginHorizontal: 20,
-      marginTop: 20,
-    },
-    input: {
-      flex: 1,
-      borderWidth: 1,
-      borderColor: '#ccc',
-      paddingHorizontal: 10,
-      paddingVertical: 5,
-      marginRight: 10,
-    },
-  });
+  form: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    marginRight: 10,
+  },
+});
 
 export default ToDoForm;
